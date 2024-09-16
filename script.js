26c32df0915ee1ed0aab37800ab50b534b5015fb
SarthakJoshi21 <157214236+SarthakJoshi21@users.noreply.github.com>
2024-09-16 22:22:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 const inputField=document.getElementById("fname");
-inputField.addEventListerner('blur',()=>{
+inputField.addEventListener('blur',()=>{
 	inputField.value = inputField.value.toUpperCase();
 })
